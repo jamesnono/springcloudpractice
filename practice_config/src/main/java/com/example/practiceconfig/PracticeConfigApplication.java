04e548036d53d31c73a7e554b0404f222a93bfda
杨锐 <rui.yang02@hand-china.com>
2019-09-26 18:49:03 +0800

    项目修改

--- practice_config/src/main/java/com/example/practiceconfig/PracticeConfigApplication.java
+++ practice_config/src/main/java/com/example/practiceconfig/PracticeConfigApplication.java
@@ -2,7 +2,12 @@ package com.example.practiceconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+@EnableDiscoveryClient
+@EnableConfigServer
 @SpringBootApplication
 public class PracticeConfigApplication {
 
